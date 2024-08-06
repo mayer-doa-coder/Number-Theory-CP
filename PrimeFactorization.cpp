3a8161e632e1d2bffd3d7d571460fc257c91e924
Tawhidul Hasan <141713816+mayer-doa-coder@users.noreply.github.com>
2024-08-07 02:42:32 +0600

    Add files via upload

---
+++ PrimeFactorization.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> PrimeFact(int n)
+{
+    vector<int> fact;
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            fact.push_back(i);
+            n=n/i;
+        }
+    }
+    if(n>1)
+        fact.push_back(n);
+    return fact;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> ans=PrimeFact(n);
+    for(auto it:ans)
+        cout<<it<<" ";
+    cout<<endl;
+}
